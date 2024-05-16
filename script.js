16297dadb932f314cace53b54c5cab636057aa35
deeprathod97 <deeprathod9797@gmail.com>
2024-05-16 12:09:39 +0530

    Introduction to arrays

---
+++ script.js
@@ -0,0 +1,13 @@
+let marks_class_12 = [91, 82, 63, 84, false, "Not Praesent"]
+
+console.log(marks_class_12)
+console.log(marks_class_12[0])
+console.log(marks_class_12[1])
+console.log(marks_class_12[2])
+console.log(marks_class_12[3])
+console.log(marks_class_12[4])
+console.log(marks_class_12[5])
+console.log(marks_class_12[6]) // Will be undefined because index 6 does not exist
+console.log("The length of marks_12 is ", marks_class_12.length)
+marks_class_12[6] = 89 // Adding a mew value to the array
+marks_class_12[0] = 96 // Adding a mew value to the array
